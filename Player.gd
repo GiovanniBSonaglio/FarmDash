d0a0c5f11c8daf72101b6c4b20f26b9e896497df
GiovanniBSonaglio <32211771+Mylordo@users.noreply.github.com>
2021-05-25 19:35:43 -0300

    Corrected spritesheet and animation

--- Player.gd
+++ Player.gd
@@ -1,9 +1,8 @@
 extends Node
 
-onready var nav_2d: Navigation2D = $Navigation2D#get_node("/root/Map/Map/Navigation2D")
+onready var nav_2d: Navigation2D = get_tree().get_root().get_node("World").get_node("Map").get_node("Navigation2D")
 onready var line_2d: Line2D = $Line2D
 onready var character: Sprite = $Character
-onready var animation: AnimationPlayer = $AnimationPlayer
 
 func _unhandled_input(event):
 	if not event is InputEventMouseButton:
@@ -12,17 +11,6 @@ func _unhandled_input(event):
 		return
 	
 	var new_path = nav_2d.get_simple_path(Vector2(character.global_position), Vector2(event.global_position), false)
-	if event.global_position.x > character.global_position.x:
-		print("Right")
-		animation.play("moveRIGHT")
-	if event.global_position.x < character.global_position.x:
-		print("Left")
-		animation.play("moveLEFT")
-	if event.global_position.y < character.global_position.y:
-		print("Up")
-		animation.play("moveUP")
-	if event.global_position.y > character.global_position.y:
-		print("Down")
-		animation.play("moveDOWN")			
+	
 	line_2d.points = new_path
 	character.path = new_path
--- Player/Character.gd
+++ Player/Character.gd
@@ -1,7 +1,10 @@
 extends Sprite
 
-var speed : = 400.0
+onready var animation: AnimationPlayer = $AnimationPlayer
+
+var speed : = 150.0
 var path : = PoolVector2Array() setget set_path
+var direction = ''
 
 func _ready():
 	set_process(false)
@@ -9,13 +12,27 @@ func _ready():
 func _process(delta: float):
 	var move_distance : = speed * delta
 	move_along_path(move_distance)
+	animate_movement()
 	
 func move_along_path(distance: float):
 	var start_point: = position
+
 	for i in range(path.size()):
 		var distance_to_next : = start_point.distance_to(path[0])
+		
 		if distance <= distance_to_next and distance >= 0.0:
 			position = start_point.linear_interpolate(path[0], distance / distance_to_next)
+			direction = path[0]
+			
+			if path[0].x > position.x:
+				direction = 'right'
+			if path[0].x < position.x:
+				direction = 'left'
+			if path[0].y < position.y:
+				direction = 'up'
+			if path[0].y > position.y:
+				direction = 'down'
+				
 			break
 		elif distance < 0.0:
 			position = path[0]
@@ -30,3 +47,13 @@ func set_path(value: PoolVector2Array):
 	if value.size() == 0:
 		return
 	set_process(true)
+
+func animate_movement():
+	if direction == 'right':
+		animation.play("moveRIGHT")
+	if direction == 'left':
+		animation.play("moveLEFT")
+	if direction == 'up':
+		animation.play("moveUP")
+	if direction == 'down':
+		animation.play("moveDOWN")
