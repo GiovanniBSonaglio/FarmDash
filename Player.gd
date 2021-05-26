3391df7fa931895da88e01ead4fce6d89958f537
Matheus Lenzi dos Santos <matheus.lenzi.santos@grad.ufsc.br>
2021-05-25 22:44:29 -0300

    Fixed mouse position bug

--- Player.gd
+++ Player.gd
@@ -5,12 +5,14 @@ onready var line_2d: Line2D = $Line2D
 onready var character: Sprite = $Character
 
 func _unhandled_input(event):
+	
+	print(get_viewport().get_mouse_position())
+	
 	if not event is InputEventMouseButton:
 		return
 	if event.button_index != BUTTON_LEFT or not event.pressed:
 		return
-	
-	var new_path = nav_2d.get_simple_path(Vector2(character.global_position), Vector2(event.global_position), false)
+	var new_path = nav_2d.get_simple_path(Vector2(character.global_position), Vector2(get_viewport().get_mouse_position()), false)
 	
 	line_2d.points = new_path
 	character.path = new_path
